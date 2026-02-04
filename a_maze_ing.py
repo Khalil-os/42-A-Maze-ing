33d7024de15cdc95583d39fbecd9b05df405776c
Khalil Riad <kriad@e1r5p12.1337.ma>
2026-02-04 15:13:47 +0100

    V1

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,17 +1,10 @@
-import sys
+from mazegen.model import Maze
 
 
 def main():
-    if (len(sys.argv) != 2):
-        print("error: you should give one configuration file")
-        sys.exit(1)
-    try:
-        with open(sys.argv[1], "r") as file:
-            content: str = file.read()
-            print(content)
-
-    except FileNotFoundError:
-        print("Error: file not found")
+    maze = Maze(10, 4)
+    maze.generate()
+    maze.display_ascii()
 
 
 if __name__ == "__main__":
